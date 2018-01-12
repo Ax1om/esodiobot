10649fcb8e02c4cdb881b89712670c284109426c
Alberto Mauri <alberto.mauri@live.com>
2018-01-12 16:39:57 +0100

    Command: Spam

--- commands/marcopolo/marcopolo.js
+++ commands/marcopolo/marcopolo.js
@@ -18,9 +18,6 @@ module.exports = bot => {
    * Marco Polo!!
    */
   bot.onText(/^\/marco/i, msg => {
-
-    console.log('marco');
-
     const isPolloExtracted = isExtracted(30);
     const imageName = isPolloExtracted ? 'pollo' : 'polo';
     const imageNumber = isPolloExtracted ? '' : Math.ceil(Math.random() * 10);
